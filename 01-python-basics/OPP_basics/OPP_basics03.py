906275af0c59ff8f2d1cbb81687eda0af10bcd3b
maulidacy <cahyamaulida011@gmail.com>
2025-09-15 11:25:18 +0700

    update

---
+++ 01-python-basics/OPP_basics/OPP_basics03.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def info(self):
+        print("\n==== Informasi Karyawan ====")
+        print(f"Nama: {self.name}")
+        print(f"Umur: {self.age}")
+        print(f"Gaji: {self.salary}")
+        print("============================")
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        print(f"Gaji {self.name} naik sebesar {amount}. Gaji baru: {self.salary}")
