b99004f015631c05231c9ef321c51ffdf4bf510e
maulidacy <cahyamaulida011@gmail.com>
2025-09-19 17:47:30 +0700

    update

--- 01-python-basics/OPP_basics/OOP_basics05.py
+++ 01-python-basics/OPP_basics/OOP_basics05.py
@@ -17,3 +17,7 @@ class Manager(Employee):
     def __init__(self, name, salary, team_size):
         super().__init__(name, salary) # panggil constructor parent class
         self.team_size = team_size
+
+    # Override method
+    def info(self):
+        print(f"{self.name}, gaji: {self.salary}, memimpin tim: {self.team_size} orang")
