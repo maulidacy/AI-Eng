6175da1a6f168912130473ede59d07430089bcb3
maulidacy <cahyamaulida011@gmail.com>
2025-09-19 17:43:11 +0700

    update

--- 01-python-basics/OPP_basics/OOP_basics05.py
+++ 01-python-basics/OPP_basics/OOP_basics05.py
@@ -2,3 +2,12 @@
 # Inheritance adalah konsep OOP yang memungkinkan sebuah kelas (class) untuk mewarisi atribut dan metode dari kelas lain.
 # Ini memungkinkan untuk membuat hierarki kelas dan mempromosikan penggunaan kembali kode (code reusability).
 # Kelas yang mewarisi disebut subclass (kelas turunan), sedangkan kelas yang diwarisi disebut superclass (kelas induk).
+
+# Parent class (superclass)
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def info(self):
+        print(f"{self.name}, gaji: {self.salary}")
