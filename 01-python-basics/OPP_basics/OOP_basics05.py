f5fc1a8e63225f469d2547b8b1166aa6592bedeb
maulidacy <cahyamaulida011@gmail.com>
2025-09-20 14:19:14 +0700

    update

--- 01-python-basics/OPP_basics/OOP_basics05.py
+++ 01-python-basics/OPP_basics/OOP_basics05.py
@@ -18,7 +18,7 @@ class Employee:
 # Child class (subclass)
 class Manager(Employee):
     def __init__(self, name, salary, team_size):
-        super().__init__(name, salary) # panggil constructor parent class
+        super().__init__(name, salary)
         self.team_size = team_size
 
     # Override method
@@ -26,11 +26,6 @@ class Manager(Employee):
         print(f"{self.name}, gaji: {self.salary}, memimpin tim: {self.team_size} orang")
 
 class Intern(Employee):
-    def bonus(self, amount):
-        # misal intern hanya boleh dapat setengah dari bonus
-        self.salary += amount / 2
-        print(f"Gaji {self.name} setelah bonus (intern): {self.salary}")
-
     def __init__(self, name, salary, duration):
         super().__init__(name, salary)
         self.duration = duration  # dalam bulan
@@ -39,9 +34,23 @@ class Intern(Employee):
     def info(self):
         print(f"{self.name}, gaji: {self.salary}, status: intern selama {self.duration} bulan")
 
+    # Override bonus method
+    def bonus(self, amount):
+        # misal intern hanya boleh dapat setengah dari bonus
+        self.salary += amount / 2
+        print(f"Gaji {self.name} setelah bonus (intern): {self.salary}")
+
 class Engineer(Employee):
-    def languages(self, langs):
-        print(f"{self.name} menguasai bahasa {', '.join(langs)}") # , ".join(langs) akan mengubah list itu jadi string yang rapi dipisahkan koma.
+    def __init__(self, name, salary, langs=None):
+        super().__init__(name, salary)
+        self.langs = langs if langs else []
+
+    def languages(self):
+        if self.langs:
+            print(f"{self.name} menguasai bahasa {', '.join(self.langs)}") # ", ".join(langs) = menggabungkan list jadi string dipisahkan koma
+
+        else:
+            print(f"{self.name} belum menambahkan bahasa yang dikuasai")
 
 # Buat objek dari kelas Employee
 emp1 = Employee("Alice", 7000000)
@@ -55,16 +64,15 @@ mgr1.info()
 emp1.bonus(500000)
 
 # Buat objek dari class Engineer
-eng1 = Engineer("Bob", 8000000)
+eng1 = Engineer("Bob", 8000000, ["Python", "JavaScript"])
 eng1.info()
-
-eng1.languages(["Python", "JavaScript"])
+eng1.languages()
 
 # Buat list employees
 employees = [
     Manager("Charlie", 12000000, 10),       # Manager
-    Engineer("David", 9000000, "Python"),   # Engineer
-    Engineer("Eva", 6000000, "Java"),       # Employee
+    Engineer("David", 9000000, ["Python"]), # Engineer
+    Engineer("Eva", 6000000, ["Java"]),     # Engineer
     Intern("Eve", 3000000, 6)               # Intern
 ]
 
