0814d14ae8aea13527b76a16aed6041e56d5adde
maulidacy <cahyamaulida011@gmail.com>
2025-09-21 14:32:55 +0700

    update

--- 01-python-basics/CLI_Sederhana/csv2json.py
+++ 01-python-basics/CLI_Sederhana/csv2json.py
@@ -3,5 +3,10 @@ from pathlib import Path
 
 # Fix the path to point to the correct location of the CSV file
 path = Path("../csv/Titanic-Dataset.csv")
+
+'''
+path.open() membuka file CSV.
+csv.DictReader() membaca CSV baris per baris dan mengubahnya jadi list of dictionary.
+'''
 rows = list(csv.DictReader(path.open(encoding='utf-8')))
 print(json.dumps(rows, indent=2, ensure_ascii=False))
