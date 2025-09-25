ca972dc6dbbe22f2f7437d540b3d6bb308bbc240
maulidacy <cahyamaulida011@gmail.com>
2025-09-25 21:36:35 +0700

    update

--- 01-python-basics/logging.py
+++ 01-python-basics/logging.py
@@ -7,4 +7,9 @@ app = FastAPI()
 @app.get("/predict")
 def ppredict(x: int):
     logging.info(f"Request received with x={x}")
+    if x < 0:
+        logging.warning(f"Input negatif, hasil mungkin tidak valid")
+    y = x * 2
+    logging.info(f"Hasil prediksi: {y}")
+    return{"hasil": y}
     
