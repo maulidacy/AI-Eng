f19d957e5f81628c80cffa1cc18ff5700ca64d26
maulidacy <cahyamaulida011@gmail.com>
2025-09-26 21:03:17 +0700

    update

--- 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
+++ 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
@@ -4,3 +4,10 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 losses = [0.9, 0.7, 0.75, 0.6, 0.55]  # Contoh data loss per epoch
+
+prev_loss = None
+for epoch, loss in enumerate(losses, start=1):
+    logging.info(f"Epoch {epoch}, lss = {loss:.2f}")
+    if prev_loss is not None and loss > prev_loss:
+        logging.warning(f"Loss naik di epoch {epoch}! ({loss:.2f} > {prev_loss:.2f})")
+        
