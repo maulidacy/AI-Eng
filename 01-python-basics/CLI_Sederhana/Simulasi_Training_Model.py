22e7c92cb940692bec3add25d1f45e4db0a28374
maulidacy <cahyamaulida011@gmail.com>
2025-09-26 21:03:45 +0700

    update

--- 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
+++ 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
@@ -10,4 +10,4 @@ for epoch, loss in enumerate(losses, start=1):
     logging.info(f"Epoch {epoch}, lss = {loss:.2f}")
     if prev_loss is not None and loss > prev_loss:
         logging.warning(f"Loss naik di epoch {epoch}! ({loss:.2f} > {prev_loss:.2f})")
-        
+    prev_loss = loss
