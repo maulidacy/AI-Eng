67f0c56eb36625fc1d7df3321f0a92726194401e
maulidacy <cahyamaulida011@gmail.com>
2025-09-26 21:00:34 +0700

    update

--- 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
+++ 01-python-basics/CLI_Sederhana/Simulasi_Training_Model.py
@@ -1,1 +1,6 @@
 import logging
+
+# Konfigurasi logging untuk menampilkan informasi di konsol (bisa atur format level dan pesan)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+losses = [0.9, 0.7, 0.75, 0.6, 0.55]  # Contoh data loss per epoch
