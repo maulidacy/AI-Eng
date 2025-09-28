646b82e01f4f2f32912a5683282c9c2a1f631e1c
maulidacy <cahyamaulida011@gmail.com>
2025-09-28 12:10:55 +0700

    update

--- 01-python-basics/tests/test_validate.py
+++ 01-python-basics/tests/test_validate.py
@@ -4,3 +4,8 @@ def test_validate_pass():
     rows = [{"nama": "Andi", "umur": 20}, {"nama": "Budi", "umur": 25}]
     result = validate(rows, ["nama", "umur"])
     assert result is True # harus lolos validasi
+
+def test_validate_missing():
+    rows = [{"nama": "Siti"}]
+    result = validate(rows, ["nama", "umur"])
+    assert result is False # harus gagal validasi
