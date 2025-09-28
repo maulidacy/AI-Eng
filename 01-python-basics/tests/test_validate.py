d606cef1e402dfb9772aeccb5cb139d00d2feec5
maulidacy <cahyamaulida011@gmail.com>
2025-09-28 12:09:46 +0700

    update

--- 01-python-basics/tests/test_validate.py
+++ 01-python-basics/tests/test_validate.py
@@ -3,4 +3,4 @@ from csv2json import validate
 def test_validate_pass():
     rows = [{"nama": "Andi", "umur": 20}, {"nama": "Budi", "umur": 25}]
     result = validate(rows, ["nama", "umur"])
-    assert result == rows
+    assert result is True # harus lolos validasi
