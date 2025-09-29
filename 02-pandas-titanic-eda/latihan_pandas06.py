6a5a6a39d3b6eb680ec9cc6c8190d9917fe26352
maulidacy <cahyamaulida011@gmail.com>
2025-09-30 06:36:53 +0700

    update

--- 02-pandas-titanic-eda/latihan_pandas06.py
+++ 02-pandas-titanic-eda/latihan_pandas06.py
@@ -38,7 +38,11 @@ mahasiswa = {
     "ipk": [3.5, 3.8, 2.9, 3.2, 3.7]
 }
 
+df = pd.DataFrame(mahasiswa)
+
 # 2. Tambahkan kolom predikat
 df["predikat"] = df["ipk"].apply(lambda x: "Cumlaude" if x >= 3.5 else ("Memuaskan" if x >= 3.0 else "Cukup"))
+logging.info("Kolom predikat ditambahkan")
+print("DataFrame setelah menambahkan kolom predikat:", df, sep="\n")
 
 
