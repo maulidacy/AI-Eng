e9adae8db2a711eb093861cc896b63b84cdeaa0d
maulidacy <cahyamaulida011@gmail.com>
2025-10-01 07:56:58 +0700

    update

--- 02-pandas-titanic-eda/latihan_pandas07.py
+++ 02-pandas-titanic-eda/latihan_pandas07.py
@@ -4,3 +4,7 @@
 - Menggunakan fungsi dasar: head(), info(), describe(), value_counts()
 - Mulai biasakan logging untuk mencatat langkah-langkah penting
 """
+
+# Baca dataset dari CSV
+import pandas as pd
+import logging
