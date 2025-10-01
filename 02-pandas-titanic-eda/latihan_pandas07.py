58222fa6a8de3abb5fac705e9edbc2f67b57a276
maulidacy <cahyamaulida011@gmail.com>
2025-10-01 07:28:02 +0700

    update

--- 02-pandas-titanic-eda/latihan_pandas07.py
+++ 02-pandas-titanic-eda/latihan_pandas07.py
@@ -2,4 +2,5 @@
 - Membaca dataset dari CSV
 - Eksplorasi awal (EDA sederhana)
 - Menggunakan fungsi dasar: head(), info(), describe(), value_counts()
+- Mulai biasakan logging untuk mencatat langkah-langkah penting
 """
