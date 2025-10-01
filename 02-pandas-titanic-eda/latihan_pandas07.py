5c89ab4bfefcd74c63050139de2dacae32e1c6ff
maulidacy <cahyamaulida011@gmail.com>
2025-10-01 08:03:10 +0700

    update

--- 02-pandas-titanic-eda/latihan_pandas07.py
+++ 02-pandas-titanic-eda/latihan_pandas07.py
@@ -9,3 +9,8 @@
 import pandas as pd
 import logging
 
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+df = pd.read_csv("../01-python-basics/csv/Titanic-Dataset.csv")
+logging.info("Dataset dibaca dengan %d baris dan %d kolom", df.shape[0], df.shape[1])
+print("5 baris pertama dataset:", df.head(), sep="\n")
