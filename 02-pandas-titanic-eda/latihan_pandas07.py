7e217f986da0fdbfa3165839c50f8eefcdd4c36e
maulidacy <cahyamaulida011@gmail.com>
2025-10-01 08:08:13 +0700

    update

--- 02-pandas-titanic-eda/latihan_pandas07.py
+++ 02-pandas-titanic-eda/latihan_pandas07.py
@@ -14,3 +14,8 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 df = pd.read_csv("../01-python-basics/csv/Titanic-Dataset.csv")
 logging.info("Dataset dibaca dengan %d baris dan %d kolom", df.shape[0], df.shape[1])
 print("5 baris pertama dataset:", df.head(), sep="\n")
+
+#----------------------------------- Eksplorasi Awal (EDA) -----------------------------------
+# Info umum
+print("\nInfo DataFrame:")
+print(df.info())
