b212f3be40fd61c3bd7d1d411f078b147042574b
maulidacy <cahyamaulida011@gmail.com>
2025-10-02 22:37:45 +0700

    update

--- 01-python-basics/CLI_Sederhana/Simple_calculator.py
+++ 01-python-basics/CLI_Sederhana/Simple_calculator.py
@@ -1,1 +1,17 @@
 # Simple calculator with validation
+def kalkulator(a_str, b_str, operator):
+    try:
+        a = float(a_str)
+        b = float(b_str)
+        if operator == "+":
+            return a + b
+        elif operator == "-":
+            return a - b
+        elif operator == "*":
+            return a * b
+        elif operator == "/":
+            return a / b
+        else:
+            return "Operator tidak valid"
+    except ValueError:
+        return "Input tidak valid"
