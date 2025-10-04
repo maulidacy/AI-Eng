c3203fa62467102a411622a0bf15d54118e489af
maulidacy <cahyamaulida011@gmail.com>
2025-10-04 12:34:48 +0700

    update

--- 01-python-basics/CLI_Sederhana/calc_cli.py
+++ 01-python-basics/CLI_Sederhana/calc_cli.py
@@ -5,3 +5,11 @@ def sub(a, b):
 def mul(a, b):
     return a * b
 def div(a, b):
+    if b == 0:
+        return "Error: Pembagian dengan nol tidak dapat dilakukan."
+    return a / b
+
+if __name__ == "__main__":
+    while True:
+        op = input("Pilih operasi [+, -, *, /] atau 'q' untuk keluar: ").strip()
+        if op == 'q':
