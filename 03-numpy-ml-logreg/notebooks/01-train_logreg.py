91bef2db8114bca5d4d29fa97f58ef79f28089b9
maulidacy <cahyamaulida011@gmail.com>
2025-10-08 21:54:28 +0700

    update

--- 03-numpy-ml-logreg/notebooks/01-train_logreg.py
+++ 03-numpy-ml-logreg/notebooks/01-train_logreg.py
@@ -9,3 +9,12 @@ X, y = iris.data, iris.target
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_Size=0.2, random_state=42)
+
+# Inisiasi model
+model = LogisticRegression(max_iter=200)
+
+# LATIH MODEL (fit)
+model.fit(X_train, y_train)
+
+# PREDIKSI (predict)
+y_pred = model.predict(X_test)
