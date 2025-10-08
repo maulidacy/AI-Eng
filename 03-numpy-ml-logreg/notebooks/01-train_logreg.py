42cf84ddd9cf97c01516f31bbb285c2e086f0640
maulidacy <cahyamaulida011@gmail.com>
2025-10-08 22:15:47 +0700

    update

--- 03-numpy-ml-logreg/notebooks/01-train_logreg.py
+++ 03-numpy-ml-logreg/notebooks/01-train_logreg.py
@@ -16,7 +16,7 @@ exit()
 # Split data (Training dan Testing)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_Size=0.2, random_state=42)
 
-# Inisiasi model
+# Inisiasi model (buat model)
 model = LogisticRegression(max_iter=200)
 
 # LATIH MODEL (fit)
