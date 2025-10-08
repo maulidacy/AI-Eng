56c532c12e00326220fa27b0450d36013a5c5a1c
maulidacy <cahyamaulida011@gmail.com>
2025-10-08 21:51:24 +0700

    update

--- 03-numpy-ml-logreg/notebooks/01-train_logreg.py
+++ 03-numpy-ml-logreg/notebooks/01-train_logreg.py
@@ -1,1 +1,8 @@
 from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# load dataset
+iris = load_iris()
+X, y = iris.data, iris.target
