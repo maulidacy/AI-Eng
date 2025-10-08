ed85ed4b152faf9344aa77db82138a100033cc35
maulidacy <cahyamaulida011@gmail.com>
2025-10-08 22:22:22 +0700

    update

--- 03-numpy-ml-logreg/notebooks/01-train_logreg.py
+++ 03-numpy-ml-logreg/notebooks/01-train_logreg.py
@@ -12,7 +12,7 @@ print("Kelas: ", iris.target_names)
 print("Ukuran dataset: ", X.shape)
 
 # Split data (Training dan Testing)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_Size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Inisiasi model (buat model)
 model = LogisticRegression(max_iter=200)
