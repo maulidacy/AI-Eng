27652c7263496913fca241d5d40bb0d66b78fd7a
maulidacy <cahyamaulida011@gmail.com>
2025-10-10 23:23:46 +0700

    update

--- 03-numpy-ml-logreg/src/train_model.py
+++ 03-numpy-ml-logreg/src/train_model.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-def train_logreg(X, y):
+def train_logreg(X, y, test_size=0.2, random_state=42):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=42)
     model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
