8049e67e14f2f5689f217a12119efd15c750f8ee
maulidacy <cahyamaulida011@gmail.com>
2025-10-11 14:33:06 +0700

    update

--- 03-numpy-ml-logreg/src/evaluate_model.py
+++ 03-numpy-ml-logreg/src/evaluate_model.py
@@ -2,7 +2,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 def evaluate_model(model, X_test, y_test, labels, report_path="report.txt"):
     y_pred = model.predict(X_test)
-    cm = confusion_matrix(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred) # matrix konfusi (y_true, y_pred)
     report = classification_report(y_test, y_pred, target_names=labels)
 
     with open(report_path, "w") as f:
