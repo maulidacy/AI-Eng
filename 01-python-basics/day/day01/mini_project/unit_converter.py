167ac03bae744fe2212f02ee2635132fb9376bcb
maulidacy <cahyamaulida011@gmail.com>
2025-10-15 06:21:44 +0700

    Update

---
+++ 01-python-basics/day/day01/mini_project/unit_converter.py
@@ -0,0 +1,8 @@
+def menu():
+    print("======Unit Converter======:")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+    print("3. Centimeters to Inches")
+    print("4. Inches to Centimeters")
+    print("5. Quit")
+    print("=========================")
