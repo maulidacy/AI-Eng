fca784e5548fdd966569bbca78dc1a282d6edccb
maulidacy <cahyamaulida011@gmail.com>
2025-10-15 06:09:32 +0700

    Update

--- 01-python-basics/day/day01/exercises/ex03_functions.py
+++ 01-python-basics/day/day01/exercises/ex03_functions.py
@@ -1,6 +1,6 @@
 def triangle_area(base: float, height: float) -> float:
     """Return area of a tringle given base and height."""
-    return 0.5 * base* height
+    return 0.5 * base * height
 
 assert abs(triangle_area(10, 2) - 10.0) < 1e-6
 print("OK")
