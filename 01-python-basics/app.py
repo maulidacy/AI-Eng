fb6bb3aece0f48c77c87d382a2eb610e0977b5f7
maulidacy <cahyamaulida011@gmail.com>
2026-01-28 14:41:00 +0700

    update

--- 01-python-basics/app.py
+++ 01-python-basics/app.py
@@ -1,6 +1,8 @@
 name = input("Nama kamu: ")
+kota = input("Kota asal kamu: ")
 age_str = input("Umur kamu (angka): ")
 
 age = int(age_str)  # ubah teks jadi angka
 print("Halo,", name)
+print("Kamu tinggal di", kota)
 print("Tahun depan umur kamu:", age + 1)
